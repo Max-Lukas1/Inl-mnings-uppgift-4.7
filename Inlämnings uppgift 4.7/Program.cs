5b404a73e03e93a61d1b8fd89511d68bf75d6e05
majo06002 <Max-Lukas@outlook.com>
2023-10-24 10:54:39 +0200

    Add project files.

---
+++ Inlämnings uppgift 4.7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Inlämningupgift4_5
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett meddelande");
+            string meddelande = Console.ReadLine();
+
+            Console.WriteLine("Ange antal steg åt höger");
+            int steg = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                for (int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(meddelande[i]);
+            }
+
+
+        }
+    }
+}
